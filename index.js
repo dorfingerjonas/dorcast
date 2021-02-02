49552a0e1c30fd66ce8cc22e6b71e3199df6e5a7
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-02 01:20:44 +0100

    implement index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const {app, BrowserWindow} = require('electron');
+const express = require('express');
+const expressApp = express();
+const http = require('http').createServer(expressApp);
+const getPort = require('get-port');
+const settings = {};
+
+(async () => {
+    settings.port = await getPort();
+    
+    http.listen(settings.port, () => {
+        console.log(`http://localhost:${settings.port}/`);
+    });
+
+    app.whenReady().then(() => {
+        expressApp.get('/', (req, res) => {
+            res.sendFile(__dirname + '/src/index.html');
+        });
+    
+        createWindow();
+    
+        app.on('activate', () => {
+            if (BrowserWindow.getAllWindows().length === 0)
+                createWindow();
+        });
+    });
+})();
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1160,
+        height: 740,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+
+    mainWindow.loadURL(`http://localhost:${settings.port}/`);
+    mainWindow.webContents.openDevTools();
+    mainWindow.focus();
+}
+
+expressApp.use(express.static(__dirname + '/src'));
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        app.quit()
+});

6bc02df688e8225160d89287d2999519b3bcdf30
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-02-02 01:19:53 +0100

    implement render.js

---
+++ src/render.js
@@ -0,0 +1,108 @@
+window.addEventListener('load', async () => {
+  const { desktopCapturer, remote } = require('electron');
+  const { writeFile } = require('fs');
+  const { dialog } = remote;
+
+  let mediaRecorder;
+  const recordedChunks = [];
+
+  const videoElement = document.querySelector('video');
+  const stopBtn = document.getElementById('stopBtn');
+  const startBtn = document.getElementById('startBtn');
+
+  printSources(await getVideoSources());
+
+  startBtn.addEventListener('click', () => {
+    mediaRecorder.start();
+    startBtn.textContent = 'Recording';
+  });
+
+  stopBtn.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startBtn.textContent = 'Start';
+  });
+
+  // Get the available video sources
+  async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+      types: ['window', 'screen']
+    });
+
+    return inputSources;
+  }
+
+  function printSources(sources) {
+    const windowWrapper = document.getElementById('windowWrapper');
+    const screenWrapper = document.getElementById('screenWrapper');
+
+    for (const source of sources) {
+      const element = document.createElement('div');
+
+      element.style.backgroundImage = `url(${source.thumbnail.toDataURL()})`;
+      element.title = source.name;
+
+      element.addEventListener('click', () => {
+        selectSource(source);
+      });
+
+      if (source.id.includes('screen')) {
+        element.classList.add('screen');
+        screenWrapper.appendChild(element);
+      } else {
+        element.classList.add('window');
+        windowWrapper.appendChild(element);
+      }
+    }
+  }
+
+  // Change the videoSource window to record
+  async function selectSource(source) {
+    const constraints = {
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'desktop',
+          chromeMediaSourceId: source.id
+        }
+      }
+    };
+
+    // Create a Stream
+    const stream = await navigator.mediaDevices
+      .getUserMedia(constraints);
+
+    // Preview the source in a video element
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+    // Create the Media Recorder
+    const options = { mimeType: 'video/webm; codecs=h264' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+  }
+
+  // Captures all recorded chunks
+  function handleDataAvailable(e) {
+    recordedChunks.push(e.data);
+  }
+
+  // Saves the video file on stop
+  async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+      type: 'video/webm; codecs=h264'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+      buttonLabel: 'Save video',
+      defaultPath: `dorcast-${Date.now()}.mp4`
+    });
+
+    if (filePath) {
+      writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+    }
+  }
+});
